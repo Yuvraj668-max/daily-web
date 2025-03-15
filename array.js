578c9dde67054bda93cf2af85e19d57b0a9c97d3
Yuvraj668-max <yuvrajkumarrkmv@gmail.com>
2025-03-15 09:27:55 +0530

    array done

--- array.js
+++ array.js
@@ -33,4 +33,19 @@ topCities.pop();
 let europeanCities = ["paris", "rome"];
 let asianCities = ["tokyo", "bangkok"];
 let worldCities = europeanCities.concat(asianCities);
-console.log(worldCities);
+// console.log(worldCities);
+
+let teaMenu = ["masala tea", "oolong tea", "green tea", "earl tea"];
+// console.log(teaMenu.length);
+
+let cityBucketList = ["kyoto", "london", "cape town", "vancouver"];
+
+let isLOndonInList = cityBucketList.includes("london");
+
+// console.log(isLOndonInList);
+
+let array1 = [1, 2, 3, 4];
+array1.shift();
+console.log(array1);
+array1.unshift(1, 2, 3, 4);
+console.log(array1);
