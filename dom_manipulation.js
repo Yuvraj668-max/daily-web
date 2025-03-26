acec9f513d6d2f0472c8c10cac27071f2a3489fd
Yuvraj668-max <yuvrajkumarrkmv@gmail.com>
2025-03-26 22:41:28 +0530

    dom manipulation 2

--- dom_manipulation.js
+++ dom_manipulation.js
@@ -21,8 +21,11 @@ document.getElementById("changeOrder").addEventListener("click", function () {
 });
 document.getElementById("addNewItem").addEventListener("click", function () {
   let newItem = document.createElement("li");
+  //   let NewItem = document.createElement("h2");
+  //   NewItem.textContent = "new heading added";
   newItem.textContent = "Pizza";
   document.getElementById("shoppingList").appendChild(newItem);
+  //   document.getElementById("example-4").appendChild(NewItem);
 });
 document
   .getElementById("removeLastTask")
@@ -30,3 +33,34 @@ document
     let tasklist = document.getElementById("Tasklist");
     tasklist.lastElementChild.remove();
   });
+document
+  .getElementById("clickMeButton")
+  .addEventListener("mouseover", function () {
+    alert("mouse hover");
+  });
+document.getElementById("teaList").addEventListener("click", function (event) {
+  alert(event.target.textContent);
+});
+document
+  .getElementById("feedbackForm")
+  .addEventListener("submit", function (event) {
+    // alert("submitted");
+    event.preventDefault();
+    let feedback = document.getElementById("feedbackInput").value;
+    document.getElementById(
+      "feedbackDisplay"
+    ).textContent = `feedback is ${feedback}`;
+
+    // console.log(event.target);
+  });
+// example 9
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("domStatus").textContent = "dom content loaded fully";
+});
+document
+  .getElementById("toggleHighlight")
+  .addEventListener("click", function () {
+    let description = document.getElementById("descriptionText");
+    description.classList.toggle("highlight");
+  });
+// example 10
