fb7a276704ae1b2d9e3dca48b9cc8a795a83eab1
Yuvraj668-max <yuvrajkumarrkmv@gmail.com>
2025-03-27 09:44:17 +0530

    asynch await

---
+++ advancedJS/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+ setTimeout(() => {
+      resolve({
+        name: "yuvraj",
+        url: "https://www.udemy.com/course/web-dev-master/learn/lecture/45708197#learning-tools",
+      });
+    }, 3000);
